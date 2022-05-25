300e3467bd1c0df87819f1d44413dd94a20b7bd2
scKim33 <102660185+scKim33@users.noreply.github.com>
2022-05-26 00:25:20 +0900

    Update model/f18_lat.py
    Co-authored-by: Hanna Lee <dlgkssk707@gmail.com>

--- model/f18_lat.py
+++ model/f18_lat.py
@@ -7,7 +7,7 @@ class f18_lon:
             self,
             x0=None,
             x_ref=None,
-            C=np.diag([1, 1, 1, 1]),
+            C=np.diag([1, 0, 0, 1]),
             Q=np.diag([1, 1, 1, 1]),
             R=np.diag([1e6, 1e6]),
             Qa=np.diag([100, 10, 10, 10, 10, 10, 10, 10]),
